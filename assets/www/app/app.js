6fe5f76f6ee9a0b5adb91e1816c7f8a3a1ccacf7
lexer <lexer.box@gmail.com>
2011-02-06 14:02:58 +0500

    add initial sencha files

---
+++ assets/www/app/app.js
@@ -0,0 +1,54 @@
+Ext.regApplication({
+            name: 'app',
+            launch: function() {
+                this.launched = true;
+                console.log('Launched');
+
+                this.views.viewport = new this.views.Viewport();
+
+            },
+            mainLaunch: function() {
+                if (!device || !this.launched) {
+                    return;
+                }
+                //console.log('mainLaunch');
+                alert('launched inside phonegap');
+            }
+        });
+
+//var PillPopper = {
+//Doctor: null,
+
+//init: function() {
+//this.initDb();
+
+//var doctor = new Doctor({name: "Krevedko"});
+//Doctor.all().add(doctor);
+
+//Doctor.all()
+//.filter("name",'=',"Krevedko")
+//.or(new persistence.PropertyFilter('name', '=', "Kurnikova"))
+//.order("name", false)
+//.list(null,function(results) {
+//	results.forEach(function (r) {
+//		console.log(r.name);
+//		});
+//});
+//},
+
+//initDb: function() {
+//	persistence.store.websql.config(persistence, 'pillpopper','pill popper database', 5 * 1024 * 1024);
+
+//persistence.reset();
+
+//	Doctor = persistence.define('Doctor', {
+//	  name: "TEXT"
+//	});
+
+//	persistence.schemaSync(function(tx) {
+// tx is the transaction object of the transaction that was
+// automatically started
+//alert("Schema Sync!");
+//	});
+//}
+//};
